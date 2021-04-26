fb3d308ef4c3e1f67d7b6537f228195d947cf566
Austin Moore <mooreat2@miamioh.edu>
2021-04-26 09:39:02 -0400

    cleans up comments and some whitespace, finishes up device creation, and starts working on swapchain/qwindow

--- main.cpp
+++ main.cpp
@@ -1,41 +1,76 @@
 #include <vulkan/vulkan.hpp>
 #include <glm/glm.hpp>
 
+// TODO fuse Qt and vulkan
+#include <QApplication>
+#include <QVulkanInstance>
+#include <QWindow>
+
 #include <algorithm>
 #include <iostream>
 #include <array>
 
-int main() {
 
-  //Basic instance setup and getting my gpu
 
+int main(int argc, char** argv) {
+
+  // Basic instance setup and getting my gpu
   vk::InstanceCreateInfo instanceInfo;
+  std::array<const char*, 2> instExtensions = {VK_KHR_SURFACE_EXTENSION_NAME, "VK_KHR_xcb_surface"};
+  instanceInfo.enabledExtensionCount = 2;
+  instanceInfo.ppEnabledExtensionNames = instExtensions.data();
   auto instance = vk::createInstance(instanceInfo);
   auto physDevices = instance.enumeratePhysicalDevices();
-  auto physGpu = physDevices[0];
+  auto physGpu = physDevices[0]; // I just hardcoded the index for my amdgpu
 
-  //
-
-
-  //Create a queue to do some work.
-  
-  std::array<float, 1> queuePriorities = {1.0f};
+  // queue to do some work.
+  std::array<float, 2> queuePriorities = {1.0f, 1.0f};
   vk::DeviceQueueCreateInfo queueInfo;
-  queueInfo.queueFamilyIndex = 0;
-  queueInfo.queueCount = 1;
+  queueInfo.queueFamilyIndex = 0; // I hardcode this queue family because I know it supports everything I want.
+  queueInfo.queueCount = 2; // I'm going to use 2 queues one for present the other for rendering.
   queueInfo.setQueuePriorities(queuePriorities);
   
-  //
-  
-  //Create a physical device
-  
+  //Create a logical device
   vk::DeviceCreateInfo deviceInfo;
   deviceInfo.queueCreateInfoCount = 1;
   deviceInfo.pQueueCreateInfos = &queueInfo;
-  deviceInfo.setEnabledExtensionCount(1);
+  deviceInfo.enabledExtensionCount = 1;
+  std::array<const char*, 1> devExtensions = {"VK_KHR_swapchain"};
+  deviceInfo.ppEnabledExtensionNames = devExtensions.data();
+  auto device = physGpu.createDevice(deviceInfo);
 
-  // TODO Finish up device creation. I think I need to enable the swapchain extension.
+  //Setup swapchain
+  vk::SurfaceKHR surface;
 
+
+
+  vk::SwapchainCreateInfoKHR swapInfo;
+  swapInfo.surface = surface;
+  //  swapInfo.imageFormat = nullptr;
+  //  swapInfo.imageColorSpace = nullptr;
+  //  swapInfo.imageExtent = nullptr;
+  swapInfo.imageArrayLayers = 1;
+  swapInfo.imageUsage = vk::ImageUsageFlagBits::eTransferDst; 
+
+
+  //Setup Qt window to handle swapchain and presentation
+  class VulkanWindow : public QWindow {
+  public:
+    VulkanWindow(vk::Instance vkinst) {
+      setSurfaceType(QSurface::VulkanSurface);
+      this->vkInstance.setVkInstance(vkinst);
+      setVulkanInstance(&this->vkInstance);
+    }
+    virtual ~VulkanWindow();
+  private:
+    QVulkanInstance vkInstance;
+    void exposeEvent(QExposeEvent*) override;
+    void resizeEvent(QResizeEvent*) override;
+    bool event(QEvent*) override;
+  };
+
+  
+    
   return 0;
 
 }
