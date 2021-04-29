7b85f88a5758d64ebef89d87e82761e831631ef4
Austin Moore <mooreat2@miamioh.edu>
2021-04-29 09:54:48 -0400

    Continues working on the pipeline

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <iostream>
 #include <array>
+#include <vulkan/vulkan_core.h>
 
 class VulkanWindow : public QWindow {
 public:
@@ -22,9 +23,9 @@ public:
   }
 private:
   QVulkanInstance qVkInstance;
-  void exposeEvent(QExposeEvent*) override;
-  void resizeEvent(QResizeEvent*) override;
-  bool event(QEvent*) override;
+  //void exposeEvent(QExposeEvent*) override;
+  //void resizeEvent(QResizeEvent*) override;
+  //bool event(QEvent*) override;
 };
 
 int main(int argc, char** argv) {
@@ -37,14 +38,16 @@ int main(int argc, char** argv) {
   vk::Instance instance{vk::createInstance(instanceInfo)};
   std::vector<vk::PhysicalDevice> physDevices{instance.enumeratePhysicalDevices()};
   vk::PhysicalDevice physDevice{physDevices[0]}; // Just hardcoded the index for a gpu that should have vulkan support.
+  //
 
-  // queue to do some work.
+  // Queue setup to do some work.
   vk::DeviceQueueCreateInfo queueInfo;
   queueInfo.queueFamilyIndex = 0; // Hardcoded this queue family because I know it supports everything I want.
   std::array<float, 1> queuePriorities{1.0f};
   queueInfo.queueCount = 1;
   queueInfo.pQueuePriorities = queuePriorities.data();
-  
+  //
+
   //Create a logical device
   vk::DeviceCreateInfo deviceInfo;
   deviceInfo.queueCreateInfoCount = 1;
@@ -53,16 +56,19 @@ int main(int argc, char** argv) {
   deviceInfo.enabledExtensionCount = devExtensions.size();
   deviceInfo.ppEnabledExtensionNames = devExtensions.data();
   vk::Device device{physDevice.createDevice(deviceInfo)};
+  //
 
   // Setup qt stuff
   QGuiApplication qapp(argc, argv);
   VulkanWindow vkWindow(instance);
   vkWindow.resize(100,100);
   vkWindow.show(); // you have to show the window before you can get it's surface.
-
+  //
+  
   // Fetch surface and info.
   vk::SurfaceKHR surface{QVulkanInstance::surfaceForWindow(&vkWindow)};
   vk::SurfaceCapabilitiesKHR surfaceCap{physDevice.getSurfaceCapabilitiesKHR(surface)};
+  //
 
   // Compute the current extent
   VkExtent2D extent{
@@ -73,6 +79,7 @@ int main(int argc, char** argv) {
                           std::min(surfaceCap.maxImageExtent.width, extent.width));
   extent.width = std::max(surfaceCap.minImageExtent.height,
                           std::min(surfaceCap.maxImageExtent.height, extent.height));
+  //
 
   // Create SwapChain
   vk::SwapchainCreateInfoKHR swapChainInfo;
@@ -90,6 +97,7 @@ int main(int argc, char** argv) {
   swapChainInfo.presentMode = vk::PresentModeKHR::eMailbox; // blindly assuming mailbox presentation mode
   swapChainInfo.clipped = VK_TRUE;
   vk::SwapchainKHR swapChain{device.createSwapchainKHR(swapChainInfo)};
+  //
 
   // Create image views
   std::vector<vk::Image> swapChainImages{device.getSwapchainImagesKHR(swapChain)};
@@ -111,6 +119,58 @@ int main(int argc, char** argv) {
                    imageInfo.subresourceRange.layerCount = 1;
                    return device.createImageView(imageInfo);
                  });
+  //
+
+  // Fixed Pipeline Setup
+  // Setup vertex input
+  vk::PipelineVertexInputStateCreateInfo vertexInputInfo;
+  vertexInputInfo.vertexBindingDescriptionCount = 0;
+  vertexInputInfo.vertexAttributeDescriptionCount = 0;
+  //
+
+  // Setup input assembly
+  vk::PipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+  inputAssemblyInfo.topology = vk::PrimitiveTopology::eTriangleList;
+  inputAssemblyInfo.primitiveRestartEnable = false;
+  //
+
+  // Setup viewport
+  vk::Viewport viewport;
+  viewport.x = 0.0f;
+  viewport.y = 0.0f;
+  viewport.width = static_cast<float>(extent.width);
+  viewport.height = static_cast<float>(extent.height);
+  viewport.minDepth = 0.0f;
+  viewport.maxDepth = 1.0f;
+  vk::Rect2D scissor;
+  scissor.offset = vk::Offset2D{0, 0};
+  scissor.extent = extent;
+  vk::PipelineViewportStateCreateInfo viewPortStateInfo;
+  viewPortStateInfo.viewportCount = 1;
+  viewPortStateInfo.pViewports = &viewport;
+  viewPortStateInfo.scissorCount = 1;
+  viewPortStateInfo.pScissors = &scissor;
+  //
+
+  // Setup rasterizer
+  vk::PipelineRasterizationStateCreateInfo rasterizerInfo;
+  rasterizerInfo.depthClampEnable = false;
+  rasterizerInfo.rasterizerDiscardEnable = false;
+  rasterizerInfo.polygonMode = vk::PolygonMode::eFill;
+  rasterizerInfo.lineWidth = 1.0f;
+  rasterizerInfo.cullMode = vk::CullModeFlagBits::eBack;
+  rasterizerInfo.frontFace = vk::FrontFace::eClockwise;
+  rasterizerInfo.depthBiasEnable = false;
+  //
+
+  // Setup multisample
+  vk::PipelineMultisampleStateCreateInfo multisamplingInfo;
+  multisamplingInfo.sampleShadingEnable = false;
+  multisamplingInfo.rasterizationSamples = vk::SampleCountFlagBits::e1;
+  //
+
+  // depth and/or stencil buffer config would happen at this stage as well, but skip it for now.
+  //
 
   return qapp.exec();
 }
